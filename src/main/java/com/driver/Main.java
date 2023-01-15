9e834ea934425bb5441a1a076e6f6b559f3df189
ASUS <aryaprasharj02@gmail.com>
2023-01-15 23:42:52 +0530

    Solution 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,26 +2,26 @@ package com.driver;
 
 public class Main {
 
-    static class Product{
 
-        public int product(int x, int y) {
-            return x * y;
-        }
-
-        public int product(int x, int y, int z) {
-            return x * y * z;
-        }
-
-        public double product(double x, double y) {
-            return x * y;
-        }
-    }
     public static void main(String[] args) {
         Product p = new Product();
-        p.product(3,4);
-        p.product(3,4,5);
-        p.product(3.4,7.1);
+        p.product(3, 4);
+        p.product(3, 4, 5);
+        p.product(3.4, 7.1);
+    }
+}
+
+ class Product{
+
+    public int product(int x, int y) {
+        return x * y;
     }
 
+    public int product(int x, int y, int z) {
+        return x * y * z;
+    }
 
+    public double product(double x, double y) {
+        return x * y;
+    }
 }
