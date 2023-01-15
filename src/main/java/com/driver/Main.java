2da404331548e584dbc797f8bd96f00d9fa33e37
ASUS <aryaprasharj02@gmail.com>
2023-01-16 00:28:10 +0530

    Sol 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,21 @@ package com.driver;
 public class Main {
 
 
+    public static class Product{
+
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
+
     public static void main(String[] args) {
         Product p = new Product();
         p.product(3, 4);
@@ -11,17 +26,4 @@ public class Main {
     }
 }
 
- class Product{
 
-    public int product(int x, int y) {
-        return x * y;
-    }
-
-    public int product(int x, int y, int z) {
-        return x * y * z;
-    }
-
-    public double product(double x, double y) {
-        return x * y;
-    }
-}
